406e0f67d6837de603f429efaae021842e917b6e
Abhishek <2021ugcs124@nitjsr.ac.in>
2022-10-26 13:57:13 +0530

    added port

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express=require('express');
 const hbs=require('hbs');
 const path=require('path');
 const app=express();
-const port=4000;
+const port=process.env.PORT || 4000;
 
 
 const static_path=(path.join(__dirname,"/public"));
